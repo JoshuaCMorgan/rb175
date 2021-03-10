3a9e0611c1afac3757516fce76f78fe97cd6b4f1
Joshua Morgan <jmorg0605@gmail.com>
2021-03-10 15:22:30 -0500

    minor fix

--- rolls.rb
+++ rolls.rb
@@ -17,22 +17,32 @@ loop do
   client = server.accept
 
   request_line = client.gets
+  puts request_line
   next if !request_line || request_line =~ /favicon/
-  client.puts(request_line)
-  
+
   http_method, path, params = parse_request(request_line)
   
-  client.puts(http_method)
-  client.puts(path)
-  client.puts(params)
+  client.puts "HTTP/1.1 200 OK"
+  client.puts "Content-Type: text/html\r\n\r\n"
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
 
   rolls = params["rolls"].to_i
   sides = params["sides"].to_i
 
-  rolls.times do
+  client.puts("<h1>Rolls!</h1>")
+  rolls.times do 
     roll = rand(sides) + 1
-    client.puts(roll)
+    client.puts("<p>", roll, "</p>")
   end
+
+  client.puts "</body>"
+  client.puts "</html>"
 
   client.close
 end

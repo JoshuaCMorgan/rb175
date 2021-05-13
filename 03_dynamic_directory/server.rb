985ee6184aa4f258954952902596f95badd6a883
Joshua Morgan <jmorg0605@gmail.com>
2021-05-13 18:35:14 -0400

    added comments

--- 03_dynamic_directory/server.rb
+++ 03_dynamic_directory/server.rb
@@ -11,3 +11,5 @@ get '/' do
   erb(:index)
 end
 
+# Sinantra allows for routes to utilize query parameters.
+# So, you can think of this route "/" as a default with the ability to expand. As a result, you can do what we did on line 8.  You can extract those parameters.

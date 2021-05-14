c331af5202601508af5e95ffc24b397408567df8
Joshua Morgan <jmorg0605@gmail.com>
2021-05-14 10:48:21 -0400

    created routes: redirect and users

--- 04_code_challenge/users.rb
+++ 04_code_challenge/users.rb
@@ -4,8 +4,18 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+before do 
+  @users = YAML.load_file("users.yaml")
+end
 
+get "/" do
+  redirect to("/home")
+end
 
+get "/users" do
+  erb(:users)
+end
 
-users = YAML.load_file("users.yaml")
-
+get "/home/:name" do
+  erb(params[:name].to_sym, {locals: params})
+end
